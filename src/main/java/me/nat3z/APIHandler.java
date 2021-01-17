3f673897339b5ea8bce951da5ce6c0ea412bddfd
Nat3z <66748576+Nat3z@users.noreply.github.com>
2021-01-16 16:35:30 -0800

    Changed to comply with GNU

--- src/main/java/me/nat3z/APIHandler.java
+++ src/main/java/me/nat3z/APIHandler.java
@@ -15,6 +15,11 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
+/*
+* I have changed the Following:
+* Error Messages if an error occured
+*/
+
 
 public class APIHandler {
 	
@@ -54,13 +59,13 @@ public class APIHandler {
 						return gson.fromJson(error, JsonObject.class);
 					}
 				} else if (urlString.startsWith("https://api.mojang.com/users/profiles/minecraft/") && conn.getResponseCode() == 204) {
-					player.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Failed with reason: Player does not exist."));
+					player.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Failed to connect to API with reason: Player does not exist."));
 				} else {
-					player.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Request failed. HTTP Error Code: " + conn.getResponseCode()));
+					player.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "API Request failed. HTTP Error Code: " + conn.getResponseCode()));
 				}
 			}
 		} catch (IOException ex) {
-			player.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "An error has occured. See logs for more details."));
+			player.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "An error when checking API has occured. See logs for more details."));
 			ex.printStackTrace();
 		}
 
